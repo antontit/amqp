edf4e153af54b7330f924a409e945086380e4d00
antontit <mail@mail.com>
2019-02-10 16:13:29 +0200

    Added signup usescase

---
+++ api/tests/Unit/Model/User/Entity/User/SingUpTest.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Test\Unit\Model\User\Entity\User;
+
+use Api\Model\User\Entity\User\ConfirmToken;
+use Api\Model\User\Entity\User\Email;
+use Api\Model\User\Entity\User\UserId;
+use Api\Model\User\Entity\User\User;
+use PHPUnit\Framework\TestCase;
+
+class SingUpTest extends TestCase
+{
+    public function testSuccess(): void
+    {
+        $user = new User(
+            $id = UserId::next(),
+            $date = new \DateTimeImmutable(),
+            $email = new Email('mail@example.com'),
+            $hash = 'hash',
+            $token = new ConfirmToken('token', new \DateTimeImmutable('+1 day'))
+        );
+        self::assertTrue($user->isWait());
+        self::assertFalse($user->isActive());
+        self::assertEquals($id, $user->getId());
+        self::assertEquals($date, $user->getDate());
+        self::assertEquals($email, $user->getEmail());
+        self::assertEquals($hash, $user->getPasswordHash());
+        self::assertEquals($token, $user->getConfirmToken());
+    }
+}
