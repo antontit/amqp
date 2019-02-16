a84f7ef758eae2124a519962cc19a3bce3ec3609
antontit <mail@mail.com>
2019-02-16 22:46:19 +0200

    Added web test case json support

--- api/tests/Feature/WebTestCase.php
+++ api/tests/Feature/WebTestCase.php
@@ -14,6 +14,7 @@ use Psr\Http\Message\ServerRequestInterface;
 use Zend\Diactoros\ServerRequest;
 use Doctrine\ORM\EntityManagerInterface;
 use Zend\Diactoros\Response;
+use Zend\Diactoros\Stream;
 use Slim\Http\Uri;
 use Slim\App;
 
@@ -24,33 +25,27 @@ class WebTestCase extends TestCase
         return $this->method($url, 'GET');
     }
 
-    protected function method(string $uri, $method): ResponseInterface
+    protected function method(string $uri, $method, array $params = []): ResponseInterface
     {
-        return $this->response(
+        $body = new Stream('php://temp', 'r+');
+        $body->write(json_encode($params));
+        $body->rewind();
+
+        return $this->request(
             (new ServerRequest())
-                ->withUri(new Uri('http', 'test' . $uri))
+                ->withHeader('Content-Type', 'application/json')
+                ->withHeader('Accept', 'application/json')
+                ->withUri(new Uri('http://test' . $uri))
                 ->withMethod($method)
+                ->withBody($body)
         );
     }
 
-    protected function response(ServerRequestInterface $request): ResponseInterface
+    protected function request(ServerRequestInterface $request): ResponseInterface
     {
         $response = $this->app()->process($request, new Response());
         $response->getBody()->rewind();
         return $response;
-    }
-
-    protected function app(): App
-    {
-        $container = $this->container();
-        $app = new App($container);
-        (require 'config/routes.php')($app);
-        return $app;
-    }
-
-    protected function container(): ContainerInterface
-    {
-        return require 'config/container.php';
     }
 
     protected function loadFixtures(array $fixtures): void
@@ -70,5 +65,18 @@ class WebTestCase extends TestCase
 
         $executor = new ORMExecutor($em, new ORMPurger($em));
         $executor->execute($loader->getFixtures());
+    }
+
+    protected function app(): App
+    {
+        $container = $this->container();
+        $app = new App($container);
+        (require 'config/routes.php')($app);
+        return $app;
+    }
+
+    protected function container(): ContainerInterface
+    {
+        return require 'config/container.php';
     }
 }
